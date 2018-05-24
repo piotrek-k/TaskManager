07afd4209dc5f85b13a45fde2930e41560edd467
Piotrek <piotrekk1@outlook.com>
2018-05-24 22:25:59 +0200

    Saving edited task details to database

--- frontend-app/src/app/task-details/task-details.component.ts
+++ frontend-app/src/app/task-details/task-details.component.ts
@@ -1,5 +1,5 @@
 import { TodoTaskDTO } from './../DTOs/TodoTaskDTO';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, ViewChild } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { TodoTasksService } from '../api-handlers/TodoTasks/todo-tasks.service';
 
@@ -14,6 +14,8 @@ export class TaskDetailsComponent implements OnInit {
   taskName: string;
   taskObject: TodoTaskDTO;
 
+  @ViewChild('taskEditTextarea') taskEditTextarea;
+
   constructor(
     public bsModalRef: BsModalRef,
     private todotaskService: TodoTasksService) { }
@@ -22,6 +24,23 @@ export class TaskDetailsComponent implements OnInit {
     this.todotaskService.getWithId<TodoTaskDTO>(this.taskId).subscribe(response => {
       this.taskObject = response;
     });
+  }
+
+  ngAfterViewInit(){
+    this.textareaAdjust();
+  }
+
+  textareaAdjust() {
+    if (this.taskEditTextarea) {
+      let o = this.taskEditTextarea.nativeElement;
+      o.style.height = "1px";
+      o.style.height = (25 + o.scrollHeight) + "px";
+    }
+  }
+
+  saveChanges() {
+    this.todotaskService.putWithId<TodoTaskDTO>(this.taskId, this.taskObject).subscribe();
+    this.bsModalRef.hide();
   }
 
 }
